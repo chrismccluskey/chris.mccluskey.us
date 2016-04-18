c636a79b61b9df22eaf29b1b13afa2ad72e69140
Chris McCluskey <chris@mccluskey.us>
2016-04-18 13:48:02 -0700

    Move main styles to main > article.road.background

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.master')
 
 @section('content')
-    <div>
+    <article class="road background">
         <p class="dark background">Are you looking for a self-motivated individual to work solo or on a team?</p>
         <p class="dark background">Does your team have junior developers who may need help reaching their full potential?</p>
         <p class="dark background">Are you a web professional who wants to collaborate on something amazing?</p>
         <p class="dark background">If you answered yes to any of these questions, we should talk.</p>
-    </div>
+    </article>
 @stop

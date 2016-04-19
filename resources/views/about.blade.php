545eadf5a9507dc2f2d0063c296064a5491d0ab5
Chris McCluskey <chris@mccluskey.us>
2016-04-19 16:38:25 -0700

    Make it look OK on mobile by cleaning up about and contact pages

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,21 +4,29 @@
   <article>
     <div class="about masthead"><h2>About Me</h2></div>
 
-    <h3>The Gist</h3>
-    <p>I'm a 33 year old who has been building for the web professionally since 2001. I've mostly worked independently, building entire web applications for private companies myself. I enjoy building entire apps solo, and I enjoy working on a team.</p>
+    <div class="solid background">
+        <h3>The Gist</h3>
+        <p>I'm a 33 year old who has been building for the web professionally since 2001. I've mostly worked independently, building entire web applications for private companies myself. I enjoy building entire apps solo, and I enjoy working on a team.</p>
+    </div>
 
-    <h3>My Ethics and Values</h3>
-    <p>I highly value equality, honesty, and accountability. All working human relationships need communication and mutual respect in order to prosper. My desire is to work amongst equals, regardless of title or skillset. I never want to stop learning and I love sharing my knowledge with eager minds.</p>
+    <div class="solid background">
+        <h3>My Ethics and Values</h3>
+        <p>I highly value equality, honesty, and accountability. All working human relationships need communication and mutual respect in order to prosper. My desire is to work amongst equals, regardless of title or skillset. I never want to stop learning and I love sharing my knowledge with eager minds.</p>
+    </div>
 
-    <h3>Some History</h3>
-    <p>I started teaching myself HTML back in 1994 when I was 12 years old; later moving on to Java and PHP during High School.</p>
+    <div class="solid background">
+        <h3>Some History</h3>
+        <p>I started teaching myself HTML back in 1994 when I was 12 years old; later moving on to Java and PHP during High School.</p>
 
-    <p>While attending college, I originally majored in Network Administration but took a job as a web developer for a local web development agency. It wasn't until I started writing full stack code in a professional setting that I decided to change my major to computer programming.</p>
+        <p>While attending college, I originally majored in Network Administration but took a job as a web developer for a local web development agency. It wasn't until I started writing full stack code in a professional setting that I decided to change my major to computer programming.</p>
 
-    <p>For more information, see <a href="/resume">my r&eacute;sum&eacute;</a>.</p>
+        <p>For more information, see <a href="/resume">my r&eacute;sum&eacute;</a>.</p>
+    </div>
 
-    <h3>Hobbies & Interests</h3>
-    <p>In my spare time I enjoy audio, video, and radio frequency manipulation. I'm interested in taking my coding skills into the hardware world and have been getting into electronics slowly.</p>
+    <div class="solid background">
+        <h3>Hobbies & Interests</h3>
+        <p>In my spare time I enjoy audio, video, and radio frequency manipulation. I'm interested in taking my coding skills into the hardware world and have been getting into electronics slowly.</p>
+    </div>
   </article>
 @stop
 
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,20 +4,23 @@
   <article>
     <div class="contact masthead"><h2>Contact Me</h2></div>
 
-    <p>Please complete the form below and I will respond as soon as possible.</p>
+    <div class="solid background">
+        <h3>Let's Talk.</h3>
+        <p>Please complete the form below and I will respond as soon as possible.</p>
 
-    <form>
-        <label for="name">Your Name</label>
-        <input type="text" name="name" />
-        <br />
-        <label for="email">Your Email Address</label>
-        <input type="text" name="email" />
-        <p>Your information is never shared and you will not be added to a mailing list.</p>
-        <br />
-        <label for="message">Your Message</label>
-        <input type="text" name="message" />
-        <br />
-        <input type="submit" value="Send Message">
-    </form>
+        <form>
+            <label for="name">Your Name</label>
+            <input type="text" name="name" />
+            <br />
+            <label for="email">Your Email Address</label>
+            <input type="text" name="email" />
+            <p>Your information is never shared and you will not be added to a mailing list.</p>
+            <br />
+            <label for="message">Your Message</label>
+            <textarea type="text" name="message"></textarea>
+            <br />
+            <input type="submit" value="Send Message">
+        </form>
+    </div>
   </article>
 @stop

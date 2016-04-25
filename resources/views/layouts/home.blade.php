d7e23bd1782faed48e3bba68984455dbb9e0df5e
Chris McCluskey <chris@mccluskey.us>
2016-04-25 13:11:32 -0700

    Force menu to show / remove menu button for desktop resolutions (closes #24)

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -5,7 +5,7 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 
       <title>Chris McCluskey - Full Stack Web Developer</title>
-      <link rel="stylesheet" type="text/css" href="/css/style.css" />
+      <link rel="stylesheet" type="text/css" href="/css/main.css" />
   </head>
   <body>
     <div id="container">

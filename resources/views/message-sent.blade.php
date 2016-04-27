ce6cde61cf92a0b66e89d585f14b83f7c1181ac6
Chris McCluskey <chris@mccluskey.us>
2016-04-27 16:12:28 -0700

    Enclose message sent text in section and solid background div (closes #38)

--- resources/views/message-sent.blade.php
+++ resources/views/message-sent.blade.php
@@ -4,9 +4,11 @@
   <article>
     <div class="contact masthead"><h2>Contact Me</h2></div>
 
-    <div class="solid background">
-        <h3>Message Sent</h3>
-        <p>Thank you for your message! If the email address you provided is valid, I will be contacting you shortly.</p>
-    </div>
+    <section>
+        <div class="solid background">
+            <h3>Message Sent</h3>
+            <p>Thank you for your message! If the email address you provided is valid, I will be contacting you shortly.</p>
+        </div>
+    </section>
   </article>
 @stop

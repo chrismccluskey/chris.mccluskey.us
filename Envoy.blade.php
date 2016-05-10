fea475fcdd94fcab47682ff4c0b976529ebffa16
Chris McCluskey <chris@mccluskey.us>
2016-05-10 13:41:16 -0700

    CI: Add deployment via Laravel Envoy from Circle CI

---
+++ Envoy.blade.php
@@ -0,0 +1,12 @@
+@setup
+    $deploy_user = $user;
+    $deploy_server = $server;
+    $deploy_server_string = sprintf('%s@%s', $deploy_user, $deploy_server);
+@endsetup
+
+@servers(['web' => $deploy_server_string])
+
+@task('deploy')
+    cd /srv/www/chris.mccluskey.us
+    git pull origin master
+@endtask
